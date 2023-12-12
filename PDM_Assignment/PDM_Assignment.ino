138590a9e890a80f8466663c875b03ae555b8de7
hat082 <ssyzz28@nottingham.edu.cn>
2023-12-12 11:34:00 +0800

    parameters for the 8 sign found

--- PDM_Assignment/PDM_Assignment.ino
+++ PDM_Assignment/PDM_Assignment.ino
@@ -6,10 +6,6 @@ int motors[4] = { 40, 40, 40, 40 };
 int num_of_laps = 0; // modify using encoder
 int lap_num = 0; // the current lap num
 
-// motors[0]  右前
-// motors[1]  右后
-// motors[2]  左前
-// motors[3]  左后
 
 void setup() {
   Wire.begin();
@@ -21,15 +17,16 @@ void setup() {
   // use button press on the lcd to confirm num of laps
 
   // debug 
-  num_of_laps = 1;
+  num_of_laps = 3;
+  // move("bafffr"); // 左后 右后 左前 右前
+  // while(1);
 }
 
 const int a = 1000;
-const int b = 1000;
+const int b = 1710;
 
 void loop() {
   delay(10);
-  lap_num++;
   if (lap_num >= num_of_laps) {
     Wire.beginTransmission(I2C_ADDRESS);
     Wire.write("ha");
@@ -38,28 +35,35 @@ void loop() {
   }
   // move forward a
   setMotorsForward();
-  move("baffff");
+  move("bafffr");
   delay(a);
   
   // turn right b
   setMotorsTurnRight();
-  move("baffff");
+  move("bafffr");
   delay(b);
+  
+  // Wire.beginTransmission(I2C_ADDRESS);
+  // Wire.write("ha");
+  // Wire.endTransmission(I2C_ADDRESS);
+  // while(1);
+
   
   // move forward 2a
   setMotorsForward();
-  move("baffff");
-  delay(2 * a);
+  move("bafffr");
+  delay(2 * a - 100);
 
   // turn left b
   setMotorsTurnLeft();
-  move("baffff");
-  delay(b);
+  move("bafffr");
+  delay(b + 130);
   
   // move forward a
   setMotorsForward();
-  move("baffff");
+  move("bafffr");
   delay(a);
+  lap_num++;  
 }
 
 void setMotorsForward() {
@@ -70,24 +74,24 @@ void setMotorsForward() {
 }
 
 void setMotorsTurnRight() {
-  motors[0] = 30;
-  motors[1] = 30;
-  motors[2] = 50;
-  motors[3] = 50;
+  motors[0] = 60;
+  motors[1] = 10;
+  motors[2] = 60;
+  motors[3] = 10;
 }
 
 void setMotorsTurnLeft() {
-  motors[0] = 50;
-  motors[1] = 50;
-  motors[2] = 30;
-  motors[3] = 30;
+  motors[0] = 10;
+  motors[1] = 60;
+  motors[2] = 10;
+  motors[3] = 60;
 }
 
 void move(char cmd[]) {
   Wire.beginTransmission(I2C_ADDRESS);
   Wire.write(cmd);
   for (int i = 0; i < 4; i++) {
-    Wire.write(abs(motors[i]));
+    Wire.write(motors[i]);
     Wire.write(0);
   }
   Wire.endTransmission();
