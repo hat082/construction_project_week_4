10c3fba80834b9b706bba821053ff6aa0de66a1e
hat082 <ssyzz28@nottingham.edu.cn>
2023-12-13 22:22:49 +0800

    Added HMI
    added encoder to control the num of laps, and a button to confirm

--- PDM_Assignment/PDM_Assignment.ino
+++ PDM_Assignment/PDM_Assignment.ino
@@ -1,16 +1,43 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
+#include <MsTimer2.h>
+#include <PinChangeInterrupt.h>
 #define I2C_ADDRESS 42
+
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+enum robotStateDef {
+  SELECT_LAPS,
+  EXECUTE_TASK,
+}robotState;
+
+volatile int encoderPinA = A0;                   // Rotary encoder Pin A
+int encoderPinB = A1;                             // Rotary encoder Pin B
+volatile int pinAStateCurrent = LOW;             // Current state of Pin A
+volatile int pinAStateLast = pinAStateCurrent;   // Last read value of Pin A
+
+int button = 8;
 
 int motors[4] = { 40, 40, 40, 40 };
 int num_of_laps = 0; // modify using encoder
 int lap_num = 0; // the current lap num
 
+void pin_init() {
+  pinMode(button, INPUT_PULLUP);
+  pinMode(encoderPinA, INPUT_PULLUP);             // Set encoderPinA as input
+  pinMode(encoderPinB, INPUT_PULLUP);             // Set encoderPinB as input
+  attachPinChangeInterrupt(digitalPinToPCINT(button), options, RISING);
+  attachInterrupt(digitalPinToInterrupt(encoderPinB), update_num_of_laps, CHANGE);
+}
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);  // Start serial for output to PC
-  // init lcd and encoder 
+  pin_init();
+  lcd.begin(16, 2);
+  robotState = SELECT_LAPS;
+  // buzzer init
 
   // display lcd screen for selecting num of laps
 
@@ -27,11 +54,50 @@ const int b = 1710;
 
 void loop() {
   delay(10);
+  
+  switch (robotState) {
+    case SELECT_LAPS:
+      lcd.setCursor(0, 0);
+      lcd.print("Laps: ");
+      lcd.print(num_of_laps);
+      lcd.setCursor(0, 1); 
+      lcd.print("Press button to confirm..")
+    case EXECUTE_TASK:
+      delay(1000);
+      executeTask();
+      robotState = SELECT_LAPS;
+      break;
+  }
+}
+
+// start executing task at the touch of the button
+void options() {
+  switch(robotState) {
+    case SELECT_LAPS:
+      robotState = EXECUTE_TASK;
+  }
+}
+
+// updates rotary encoder values if there are any detected
+void update_num_of_laps() {
+  pinAStateCurrent = digitalRead(encoderPinA);   // Read the current state of Pin A
+
+  // If there is a minimal movement of 1 step
+  if ((pinAStateLast == HIGH) && (pinAStateCurrent == LOW)) {
+    if (digitalRead(encoderPinB) == HIGH) {      // If encoderPinB is HIGH
+      num_of_laps = constrain(num_of_laps + 1, 0, 10);
+    } else {
+      num_of_laps = constrain(num_of_laps - 1, 0, 10);
+    }
+  }
+  pinAStateLast = pinAStateCurrent;              // Store the latest read value in the current state variable
+}
+
+void executeTask() {
   if (lap_num >= num_of_laps) {
     Wire.beginTransmission(I2C_ADDRESS);
     Wire.write("ha");
     Wire.endTransmission(I2C_ADDRESS);
-    while(1);
   }
   // move forward a
   setMotorsForward();
@@ -63,7 +129,7 @@ void loop() {
   setMotorsForward();
   move("bafffr");
   delay(a);
-  lap_num++;  
+  lap_num++; 
 }
 
 void setMotorsForward() {
